b7b078968d6e5972776c4840d57f466715849c41
Marco Rodriguez Melgarejo <mrodrguezmelgarejo@frba.utn.edu.ar>
2022-10-31 23:08:39 -0300

    Arreglo errores indices

--- creacion_de_indices.sql
+++ creacion_de_indices.sql
@@ -1,13 +1,9 @@
 /*create index index_name
 on table_name(column1, column2, .., columnN);*/
 
-create index indice_por_nombre
-on nibble.Canal(nombre);
 
 -- creamos estos dos porque las personas que usen el sistema van a querer buscar por nombre (el id es generado)
 -- y no impacta tanto el rendimiento porque casi nunca se van a crear filas en estas tablas
-
-
 create index indice_por_nombre
 on nibble.Canal(nombre);
 
@@ -21,7 +17,7 @@ create index indice_por_nombre
 on nibble.Provincia(nombre);   
 
 create index indice_por_nombre
-on nibble.medio_de_envio(nombre);   
+on nibble.medio_envio(nombre);   
 
 -- medio dudoso xq capaz crean muchos productos al ser un negocio de ropa
 -- pero la gente probablemente quiera buscar por nombre al comprar asi que lo ponemos
